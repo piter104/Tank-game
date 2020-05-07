ffe0df7d486a6f0092fb799246db13b6a77e4308
lukasz1303 <56319763+lukasz1303@users.noreply.github.com>
2020-05-07 09:28:18 +0200

    poprawki

---
+++ shaders/f_particle.glsl
@@ -0,0 +1,10 @@
+#version 330
+
+
+out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
+in vec4 iC;
+
+void main(void) {
+
+	pixelColor = iC;
+}
---
+++ shaders/v_particle.glsl
@@ -0,0 +1,19 @@
+#version 330
+
+//Zmienne jednorodne
+uniform mat4 P;
+uniform mat4 V;
+uniform mat4 M;
+
+//Atrybuty
+in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
+in vec4 color;
+
+out vec4 iC;
+
+void main(void) {
+
+    iC = color;
+    gl_Position=P*V*M*vertex;
+
+}
